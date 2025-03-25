c48c17a7c53cba3875e66bb8d6c6b04be16a1d1a
deesh <deesh@LAPTOP-J3TF46SF>
2025-03-25 18:37:11 +0530

    Added Test Suite

---
+++ Test Suites/SmokeSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SmokeSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2c95437e-e291-4759-bfbc-78178c7f4953</testSuiteGuid>
+   <testCaseLink>
+      <guid>6ebf676b-2049-4098-a150-04cc51ae3b0a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home_Page/ValidateHomePageManual</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
